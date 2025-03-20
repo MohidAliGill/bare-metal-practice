427956d7e3fe357d3d9f13c847fe7278bf6fab79
Mohid Ali Gill <mohidali55@gmail.com>
2025-03-20 17:44:47 +0000

    board changed to Arduino

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # bare-metal-practice
 
 Practicing bare metal concepts
+
+Development Board used: Arduino Uno
+
+## Libraries Required
+avr-libc
+avrdude 
+binutils-avr 
+gcc-avr
+
+On mac-os can be installed using `brew`
--- blinky/blinky.c
+++ blinky/blinky.c
@@ -1,48 +1,23 @@
-#include <stdint.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
-#define GPIO_BASE       0x50000000
-#define GPIO_DIR        (*(volatile uint32_t *)(GPIO_BASE + 0x518))
-#define GPIO_OUT        (*(volatile uint32_t *)(GPIO_BASE + 0x504))
-#define LED_PIN         13
+#define DELAY_TIME      500
 
-#define SYSTICK_BASE    0xE000E010
-#define SYSTICK_CTRL    (*(volatile uint32_t *)(SYSTICK_BASE + 0x00))
-#define SYSTICK_LOAD    (*(volatile uint32_t *)(SYSTICK_BASE + 0x04))
-#define SYSTICK_VAL     (*(volatile uint32_t *)(SYSTICK_BASE + 0x08))
-
-#define __WFI() __asm volatile ("wfi")
-
-volatile uint32_t sysTickCounter = 0;
-
-void SysTick_Handler(void)
+static void blink()
 {
-    sysTickCounter++;
-}
+    PORTB |= (1 << PORTB5);
+    _delay_ms(DELAY_TIME);
 
-void systick_init(void)
-{
-    SYSTICK_LOAD = 64000 - 1;
-    SYSTICK_VAL = 0;
-    SYSTICK_CTRL = 7;
-}
-
-void delay_ms(uint32_t ms)
-{
-    sysTickCounter = 0;
-    while (sysTickCounter < ms)
-    {
-        __WFI();
-    }
+    PORTB = PORTB & ~(1 << PORTB5);
+    _delay_ms(DELAY_TIME);
 }
 
 int main(void)
 {
-    systick_init();
-    GPIO_DIR |= (1 << LED_PIN);
+    DDRB |= (1 << DDB5);
 
     while (1)
     {
-        GPIO_OUT ^= (1 << LED_PIN);
-        delay_ms(500);
+        blink();
     }
 }
