5119e3ed024bb69826c92df41637f827933cbce3
Mohid Ali Gill <mohidali55@gmail.com>
2025-03-20 13:17:23 +0000

    used systick

--- blinky/blinky.c
+++ blinky/blinky.c
@@ -5,18 +5,39 @@
 #define GPIO_OUT        (*(volatile uint32_t *)(GPIO_BASE + 0x504))
 #define LED_PIN         13
 
-void delay(uint32_t ms)
+#define SYSTICK_BASE    0xE000E010
+#define SYSTICK_CTRL    (*(volatile uint32_t *)(SYSTICK_BASE + 0x00))
+#define SYSTICK_LOAD    (*(volatile uint32_t *)(SYSTICK_BASE + 0x04))
+#define SYSTICK_VAL     (*(volatile uint32_t *)(SYSTICK_BASE + 0x08))
+
+volatile uint32_t sysTickCounter = 0;
+
+void SysTick_Handler(void)
 {
-    for (volatile uint32_t i = 0; i < (ms * 1000); i++);
+    sysTickCounter++;
+}
+
+void systick_init(void)
+{
+    SYSTICK_LOAD = 64000 - 1;
+    SYSTICK_VAL = 0;
+    SYSTICK_CTRL = 7;
+}
+
+void delay_ms(uint32_t ms)
+{
+    sysTickCounter = 0;
+    while (sysTickCounter < ms);
 }
 
 int main(void)
 {
+    systick_init();
     GPIO_DIR |= (1 << LED_PIN);
 
     while (1)
     {
         GPIO_OUT ^= (1 << LED_PIN);
-        delay(500);
+        delay_ms(500);
     }
 }
