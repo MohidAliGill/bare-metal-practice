09ceb42e386a7dd167ad493fa1f966c8e33c4149
Mohid Ali Gill <mohidali55@gmail.com>
2025-03-22 13:16:30 +0000

    code refactored

--- led-brightness-with-pwm/led-brightness-with-pwm.c
+++ led-brightness-with-pwm/led-brightness-with-pwm.c
@@ -1,13 +1,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define F_CPU       16000000UL
-#define PWM_TOP     255
+#define F_CPU           16000000UL
+#define PWM_TOP         255     // 8-bit resolution
+#define PWM_PRESCALER   8
+#define FADE_DELAY_MS   20
 
+// Initialize Timer1 in Fast PWM mode (Mode 14)
 static void init_pwm()
 {
-    DDRB |= (1 << PORTB1);
+    DDRB |= (1 << PB1); // Pin 9  as Output pin
     
+    // Fast PWM Mode 14: WGM13:WGM10 = 1110
     TCCR1A = (1 << COM1A1) | (1 << WGM11);
     TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Fast PWM - Prescaler = 8
 
@@ -16,6 +20,7 @@ static void init_pwm()
     OCR1A = PWM_TOP / 2;
 }
 
+// Set PWM duty cycle on OC1A (PB1)
 void set_pwm_duty(uint8_t duty)
 {
     OCR1A = duty;
